26dc99c61f3234859ac3f558d5328cb999ca822c
EunkyoungKim <dossiba@naver.com>
2019-10-04 21:38:02 +0900

    Revert "pug를 이용하여 view 작성"
    This reverts commit 2caa3c9d00453d26555ac08c3e9391d2c4815a27.

--- controllers/writingController.js
+++ controllers/writingController.js
@@ -1,5 +1,5 @@
-export const writing = (req, res) => res.render("writing");
-export const upload = (req, res) => res.render("upload");
-export const writingDetail = (req, res) => res.render("writingDetail");
-export const editWriting = (req, res) => res.render("editWriting");
-export const deleteWriting = (req, res) => res.render("deleteWriting");
+export const writing = (req, res) => res.send("Writing");
+export const upload = (req, res) => res.send("Upload");
+export const writingDetail = (req, res) => res.send("Writing Detail");
+export const editWriting = (req, res) => res.send("Edit writing");
+export const deleteWriting = (req, res) => res.send("Delete Writing");
